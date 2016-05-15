dcd720120897e85b4596d987874fabb23556c8f3
jacobben85 <jacobben85@gmail.com>
2016-05-14 21:21:09 -0400

    Initial pom

---
+++ src/main/java/com/jbjohn/AggregatorApplication.java
@@ -0,0 +1,12 @@
+package com.jbjohn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregatorApplication.class, args);
+	}
+}
