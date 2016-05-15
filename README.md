f7f60dd17c544ddebb2726dfb7284bf4b797d074
jacobben85 <jacobben85@gmail.com>
2016-05-14 21:49:48 -0400

    Pom updates and class namings

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # aggregator
+Aggregator
--- src/main/java/com/jbjohn/Aggregator.java
+++ src/main/java/com/jbjohn/Aggregator.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AggregatorApplication {
+public class Aggregator {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AggregatorApplication.class, args);
+		SpringApplication.run(Aggregator.class, args);
 	}
 }
